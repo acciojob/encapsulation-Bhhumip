9c7f493a17525f8e50f67b1e6054cfc2c8276728
Bhhumip <bhumikapawar41@gmail.com>
2026-07-13 21:52:37 +0530

    comment removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
           RWOnly obj = new RWOnly();
-          //System.out.println(obj.name);
-        //Error-> java: name has private access in RWOnly
+
         obj.setName("Bhumi");
         System.out.println(obj.getName());
     }
